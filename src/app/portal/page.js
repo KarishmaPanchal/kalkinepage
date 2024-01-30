3bcd975324a0b943f560dd350d9f30db8eb9496e
Karishma Panchal <karishma.panchal@kalkine.com>
2024-01-30 16:08:26 +0530

    newfile

--- src/app/portal/page.js
+++ src/app/portal/page.js
@@ -14,13 +14,48 @@ import Graphsssss from "../../../public/static/image/graph6.png";
 import Lock from "../../../public/static/image/lock.png";
 import Arrow from "../../../public/static/image/arrow.png";
 import Arrownew from "../../../public/static/image/arrow2.png";
+import Article from "../../../public/static/image/latestarticleone.png";
 import { MdArrowUpward } from "react-icons/md";
+import { IoIosContact } from "react-icons/io";
+import { IoIosArrowRoundDown } from "react-icons/io";
 
 import { HiShare } from "react-icons/hi";
 import { IoIosArrowRoundUp } from "react-icons/io";
 export default function Portal() {
   return (
     <div>
+      <div className="container-fluid tabnavgation">
+        <div className="container">
+        <Row>
+            <Col span={24}>
+              <ul className="company_navagation">
+                <li>
+                  <a href="#company_overview" className="active">Overview</a>
+                </li>
+                <li>
+                  <a href="#company_overview" className="active">Financial</a>
+                </li>
+                <li>
+                  <a href="#company_overview" className="active">Peer Comparsion</a>
+                </li>
+                <li>
+                  <a href="#company_overview" className="active">Reports</a>
+                </li>
+                <li>
+                  <a href="#company_overview" className="active">Stock Research & News</a>
+                </li>
+                <li>
+                  <a href="#company_overview" className="active">Profile</a>
+                </li>
+
+              </ul>
+              
+            </Col>
+          </Row>
+
+        </div>
+
+      </div>
       <div className="container-fluid">
         <div className="container">
           <Row>
@@ -35,6 +70,7 @@ export default function Portal() {
                     <HiShare />
                     Share
                   </li>
+                
                 </ul>
                 <div className="name">
                   <h1 className="companyname">Constellation Software Inc.</h1>
@@ -53,8 +89,7 @@ export default function Portal() {
                           <div className="topp">
                             <span className="gain">1.60</span>
                             <span className="gainpercentage">
-                              (
-                              <IoIosArrowRoundUp />
+                              (<IoIosArrowRoundUp />
                               0.04%)
                             </span>
                           </div>
@@ -71,15 +106,9 @@ export default function Portal() {
                             <span>3600.00</span>
                             <span>3642.00</span>
                           </div>
-                          <input
-                            type="range"
-                            min="3600.00"
-                            max="3642.00"
-                            value="3623.00"
-                            class="range3"
-                            step="0.001"
-                            disabled=""
-                          ></input>
+                       
+                            <input type="range" step="0.001" id="volume"  className="range3" name="volume" min="3600.00" max="3642.00"/>
+
                           <div className="rangespan">
                             <span>Low</span>
                             <span>High</span>
@@ -127,7 +156,7 @@ export default function Portal() {
                       <li class="nav-item">
                         <a
                           href="#keyratios"
-                          class="nav-link"
+                          class="nav-link active"
                           data-bs-toggle="tab"
                         >
                           Charts
@@ -136,7 +165,7 @@ export default function Portal() {
                       <li class="nav-item">
                         <a
                           href="#incomestatement"
-                          class="nav-link active"
+                          class="nav-link "
                           data-bs-toggle="tab"
                         >
                           Income Statement
@@ -309,8 +338,8 @@ export default function Portal() {
                       </td>
                       <td className="text-end">
                         -0.12
-                        <span className="similarcomp_percent isnegative">
-                          0.09%
+                        < span className="similarcomp_percent isnegative">
+                        <IoIosArrowRoundDown/>0.09%
                         </span>
                       </td>
                       <td className="text-end">132.22</td>
@@ -422,88 +451,266 @@ export default function Portal() {
                 </table>
               </div>
             </Col>
-            <Row>
-              <Col span={24}>
-                <div className="reportcoveredsection">
-                  <h2 className="sectionheadings">Report Covered</h2>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <div className="reportcoveredsection">
+                <h2 className="sectionheadings">Report Covered</h2>
 
-                  <div className="datamapp">
-                    {[1, 2, 3, 4, 5, 6, 7].map((i) => {
-                      return (
-                        <>
-                          <div className="item">
-                            <div className="reportcovered_data">
-                              <p className="compreport_stats">
-                                <span className="reportcomp_category">
-                                  Small-Cap
+                <div className="datamapp">
+                  {[1, 2, 3, 4, 5, 6, 7].map((i) => {
+                    return (
+                      <>
+                        <div className="item">
+                          <div className="reportcovered_data">
+                            <p className="compreport_stats">
+                              <span className="reportcomp_category">
+                                Small-Cap
+                              </span>
+                              <span>High</span>
+                            </p>
+                            <h3>
+                              <a href="/">Medifast, Inc</a>
+                            </h3>
+                            <p className="reporttime">
+                              3&nbsp;days, 22&nbsp;hours ago
+                            </p>
+                            <p className="reportcontent_stats">
+                              <span className="reportcomp_price">
+                                <label>Rec. Price</label>
+                                {/* <span className="recommended_value">US$_</span> */}
+                                <Image src={Lock} />
+                              </span>
+                              <span className="reportcomp_action">
+                                <label>Action</label>
+                                <span className="ispositive">
+                                  {/* <span className="actionrecomendation">_</span> */}
                                 </span>
-                                <span>High</span>
-                              </p>
-                              <h3>
-                                <a href="/">Medifast, Inc</a>
-                              </h3>
-                              <p className="reporttime">
-                                3&nbsp;days, 22&nbsp;hours ago
-                              </p>
-                              <p className="reportcontent_stats">
-                                <span className="reportcomp_price">
-                                  <label>Rec. Price</label>
-                                  {/* <span className="recommended_value">US$_</span> */}
-                                  <Image src={Lock} />
-                                </span>
-                                <span className="reportcomp_action">
-                                  <label>Action</label>
-                                  <span className="ispositive">
-                                    {/* <span className="actionrecomendation">_</span> */}
-                                  </span>
-                                  <Image src={Lock} />
-                                </span>
-                              </p>
-                            </div>
+                                <Image src={Lock} />
+                              </span>
+                            </p>
                           </div>
-                        </>
-                      );
-                    })}
-                  </div>
-                  <div className="owl-nav">
-                    <button type="button" className="owl-prev">
-                      <Image src={Arrow} />
-                    </button>
-                    <button type="button" className="owl-next disabled">
-                      <Image src={Arrownew} />
-                    </button>
-                  </div>
+                        </div>
+                      </>
+                    );
+                  })}
                 </div>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={24}>
-                <h2 className="sectionheadings">Stock Research & News</h2>
-                <div className="tab">
-                  <ul class="nav nav-tabs kalgainers_hometabs">
-                    <li class="nav-item">
-                      <a
-                        href="#stockresearch"
-                        class="nav-link active"
-                        data-bs-toggle="tab"
-                      >
-                        Stock Research
+                <div className="owl-nav">
+                  <button type="button" className="owl-prev">
+                    <Image src={Arrow} />
+                  </button>
+                  <button type="button" className="owl-next disabled">
+                    <Image src={Arrownew} />
+                  </button>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <h2 className="sectionheadings">Stock Research & News</h2>
+              <div className="tab">
+                <ul class="nav nav-tabs kalgainers_hometabs">
+                  <li class="nav-item">
+                    <a
+                      href="#stockresearch"
+                      class="nav-link active"
+                      data-bs-toggle="tab"
+                    >
+                      Stock Research
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      href="#trendingnewsarticle"
+                      class="nav-link"
+                      data-bs-toggle="tab"
+                    >
+                      News
+                    </a>
+                  </li>
+                </ul>
+                <a href="/" class="newsall">View All</a>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <div className="tab-content articlenewstab">
+                <div className="tab-pane fade show active">
+                  <ul className="latestarticleul">
+                    <li>
+                      <a href="/">
+                        <Image src={Article} />
+                        <div className="articlecontent">
+                          <div className="newstiming">
+                            <p>17&nbsp;hours ago | Team Kalkine</p>
+                          </div>
+                          <h2>
+                            One Materials Stock to Consider at the Current
+                            Level- BTO
+                          </h2>
+                        </div>
                       </a>
                     </li>
-                    <li class="nav-item">
-                      <a
-                        href="#trendingnewsarticle"
-                        class="nav-link"
-                        data-bs-toggle="tab"
-                      >
-                        News
+                    <li>
+                      <a href="/">
+                        <Image src={Article} />
+                        <div className="articlecontent">
+                          <div className="newstiming">
+                            <p>17&nbsp;hours ago | Team Kalkine</p>
+                          </div>
+                          <h2>
+                            One Materials Stock to Consider at the Current
+                            Level- BTO
+                          </h2>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <Image src={Article} />
+                        <div className="articlecontent">
+                          <div className="newstiming">
+                            <p>17&nbsp;hours ago | Team Kalkine</p>
+                          </div>
+                          <h2>
+                            One Materials Stock to Consider at the Current
+                            Level- BTO
+                          </h2>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <Image src={Article} />
+                        <div className="articlecontent">
+                          <div className="newstiming">
+                            <p>17&nbsp;hours ago | Team Kalkine</p>
+                          </div>
+                          <h2>
+                            One Materials Stock to Consider at the Current
+                            Level- BTO
+                          </h2>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <Image src={Article} />
+                        <div className="articlecontent">
+                          <div className="newstiming">
+                            <p>17&nbsp;hours ago | Team Kalkine</p>
+                          </div>
+                          <h2>
+                            One Materials Stock to Consider at the Current
+                            Level- BTO
+                          </h2>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/">
+                        <Image src={Article} />
+                        <div className="articlecontent">
+                          <div className="newstiming">
+                            <p>17&nbsp;hours ago | Team Kalkine</p>
+                          </div>
+                          <h2>
+                            One Materials Stock to Consider at the Current
+                            Level- BTO
+                          </h2>
+                        </div>
                       </a>
                     </li>
                   </ul>
-                  <a href="/" className="newsall">View all</a>
                 </div>
-              </Col>
-            </Row>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <h2 className="sectionheadings">Profile</h2>
+              <div className="descriptionbox">
+                <input type="checkbox" />
+                <p className="compprofile_description">
+                  TerrAscend Corp. cultivates, processes, and sells medical and
+                  adult use cannabis in Canada and the United States. The
+                  company produces and distributes hemp-derived wellness
+                  products to retail locations; and manufactures
+                  cannabis-infused artisan edibles. It also operates retail
+                  dispensaries under the Apothecarium brand name in California,
+                  Pennsylvania, and New Jersey. In addition, the company owns
+                  various synergistic businesses under Gage Growth, Pinnacle,
+                  TerrAscend NJ, Ilera Healthcare, HMS Health and HMS
+                  Processing, Valhalla Confections, State Flower, Arise
+                  Bioscience Inc., and TerrAscend Canada brands. It serves
+                  medical and legal adult-use markets. The company was
+                  incorporated in 2017 and is headquartered in Mississauga,
+                  Canada.
+                </p>
+
+                <label
+                  for="expanded"
+                  role="button"
+                  id="ob-readmore-placeholder"
+                >
+                  <span></span>
+                </label>
+              </div>
+              <h3 className="sectionsubheadings">TerrAscend Corp</h3>
+              <div className="contact">
+                <ul className="comprofile_contact">
+                  <li>
+                    <IoIosContact />
+                    Employees :<b>972</b>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+            <Col span={12}>
+              <h3 className="sectionsubheadings">Key Executives</h3>
+              <div className="table-executive">
+                <div className="table-responsive">
+                  <table class="table text-nowrap" id="directors-transections">
+                    <thead>
+                      <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Year Born</th>
+                      </tr>
+                    </thead>
+                    <tbody></tbody>
+                  </table>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <p className="compprofile_description">
+                <b>Disclaimer -</b> Data Powered by EOD Historical Data
+                (“EODHD”). All CFDs (stocks, indices, mutual funds, ETFs), and
+                Forex are not provided by exchanges but rather by market makers,
+                and so prices may not be accurate and may differ from the actual
+                market price, meaning prices are indicative and not appropriate
+                for trading purposes. EODHD is not using exchanges data feeds
+                for the pricing data, EODHD is using OTC, peer to peer trades
+                and trading platforms over 100+ sources, EODHD is aggregating
+                its data feeds via VWAP method. Therefore, EOD Historical Data
+                doesn’t bear any responsibility for any trading losses you might
+                incur as a result of using this data. EOD Historical Data or
+                anyone involved with EOD Historical Data will not accept any
+                liability for loss or damage as a result of reliance on the
+                information including data, quotes, charts and buy/sell signals
+                contained within this website. Please be fully informed
+                regarding the risks and costs associated with trading the
+                financial markets, it is one of the riskiest investment forms
+                possible. EOD Historical Data does not give any warranties
+                (including, without limitation, as to merchantability or fitness
+                for a particular purpose or use).
+              </p>
+            </Col>
           </Row>
         </div>
       </div>
